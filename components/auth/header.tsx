261381144bc067dedf14172a71a2b92260e807c4
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-03-14 17:12:30 +0800

    Update header styling

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -4,8 +4,8 @@ type HeaderProps = {
 
 export const Header = ({ label }: HeaderProps) => {
   return (
-    <div className="w-full flex flex-col gap-y-4">
-      <h1 className="text-2xl font-semibold">{label}</h1>
+    <div className="w-full flex flex-col">
+      <h1 className="text-xl text-center font-semibold">{label}</h1>
     </div>
   )
 }

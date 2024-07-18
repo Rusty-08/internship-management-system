7e8ec1edb583e47011853e81d428db351b909930
Jonathan Goboli <jgoboli2@gmail.com>
2024-07-18 21:19:52 +0800

    Fix: Filter attendance records by matching formatted dates

--- app/admin/interns-attendance/_components/attendance.tsx
+++ app/admin/interns-attendance/_components/attendance.tsx
@@ -1,26 +1,33 @@
 'use client'
 
-import { AttendanceProps, attendanceColumns } from '@/app/intern/my-attendance/_components/attendance-columns'
-import AttendanceTable from '@/app/intern/my-attendance/_components/attendance-table'
-import React, { useState } from 'react'
 import {
-  isEqual,
-  format
-} from 'date-fns'
+  AttendanceProps,
+  attendanceColumns,
+} from '@/app/intern/my-attendance/_components/attendance-columns'
+import AttendanceTable from '@/app/intern/my-attendance/_components/attendance-table'
 import { DayPicker } from '@/components/@core/ui/day-picker'
 import { ColumnDef } from '@tanstack/react-table'
+import { format, isEqual } from 'date-fns'
+import React, { useState } from 'react'
 
 type AdminAttendanceProps = {
   currentAttendance: AttendanceProps[]
   isInDashboard?: boolean
 }
 
-const Attendance = ({ currentAttendance, isInDashboard = false }: AdminAttendanceProps) => {
+const Attendance = ({
+  currentAttendance,
+  isInDashboard = false,
+}: AdminAttendanceProps) => {
   const [date, setDate] = useState<Date | undefined>(new Date())
-  
-  const filteredAttendance = date ? (currentAttendance.filter(
-    attendance => format(attendance.date, 'PPP') === format(date, 'PPP')
-  )) : []
+
+  const filteredAttendance = date
+    ? currentAttendance.filter(
+        attendance =>
+          attendance.date &&
+          format(attendance.date, 'PPP') === format(date, 'PPP'),
+      )
+    : []
 
   const dashboardAttendance = (attendance: ColumnDef<AttendanceProps>[]) => {
     const _attendance = [...attendance]
@@ -30,14 +37,15 @@ const Attendance = ({ currentAttendance, isInDashboard = false }: AdminAttendanc
 
   const attendanceCols = dashboardAttendance(attendanceColumns)
 
-  const columns = 
-    isInDashboard 
+  const columns = isInDashboard
     ? attendanceCols.slice(0, attendanceCols.length - 1)
     : attendanceCols
 
   return (
     <div className="flex flex-col gap-4">
-      {!isInDashboard && <DayPicker date={date || undefined} setDate={setDate} />}
+      {!isInDashboard && (
+        <DayPicker date={date || undefined} setDate={setDate} />
+      )}
       <AttendanceTable
         data={filteredAttendance}
         showTimeInBtn={false}
